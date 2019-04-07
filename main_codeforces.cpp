8022843e8a092d71bccbeb80956c7b9d364e283c
Li-Hung Chen <badstyle319@hotmail.com>
2019-04-07 22:00:10 +0800

    sync files

---
+++ main_codeforces.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <bitset>
+#include <ctime>
+
+#define LL long long
+#define ULL unsigned long long
+#define PI 3.14159265
+
+using namespace std;
+
+static int dx[] = {-1,-1,-1,0,0,1,1,1};
+static int dy[] = {-1,0,1,-1,1,-1,0,1};
+
+#define FILENAME "sample"
+
+void solve(){
+	
+}
+
+int main()
+{
+#ifdef D
+	freopen(FILENAME ".in", "r", stdin);
+	freopen(FILENAME ".out", "w", stdout);
+#endif
+	
+	
+	
+#ifdef D
+	fclose(stdin);
+	fclose(stdout);
+#endif
+
+	return 0;
+}
